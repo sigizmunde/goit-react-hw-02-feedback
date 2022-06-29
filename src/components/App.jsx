7748c9cb257dc89de3e7919c777961144de4d810
illya bondar <1816579@gmail.com>
2022-06-30 02:18:35 +0300

    unused render removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import { Box, Caption } from "./App.styled.js";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions.jsx";
